bceb3ac3c532eeebf214283ea3e8eb15119b5421
Bancosta <lbancosta@dxc.com>
2019-08-26 21:39:04 +0800

    rebase EmpService

---
+++ backend/dso-microservices/EmpService/src/main/java/com/dxc/dtc/dso/emp/repository/EmpRepository.java
@@ -0,0 +1,17 @@
+package com.dxc.dtc.dso.emp.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.dxc.dtc.dso.emp.model.Emp;
+
+@RepositoryRestResource(collectionResourceRel = "empployees", path = "empployees")
+public interface EmpRepository extends PagingAndSortingRepository<Emp, Long>{
+	
+	List<Emp> findByEmpUsername(@Param("empUsername") String empUsername);
+	
+	List<Emp> findByEmpEmail(@Param("empEmail") String empEmail);
+}
